a5355c443a580cdda1451f51f987bcafa76b334e
Archit <archit.raj@unisys.com>
2020-06-26 22:09:04 +0530

    initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DrawingView dv;
+
+        dv = new DrawingView(this);
+        setContentView(dv);
+
+        dv.setDrawColor(Color.RED);
+
+    }
+}
